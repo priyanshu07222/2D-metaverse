d4f4b7281ac059060f9e55b74055b5393216c499
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-11-11 15:46:14 +0530

    init Db & created schema

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
 export const router = Router()
 
@@ -14,3 +15,4 @@ router.get('/avatars', () => { })
 
 router.use('/user', userRouter)
 router.use('/space', spaceRouter)
+router.use('/admin', adminRouter)
