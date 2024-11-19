cb2d376e210151a17ed6b16aecf4eb478a4053a0
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-11-19 23:05:21 +0530

    added phaser

--- metaverse/apps/frontend/src/App.tsx
+++ metaverse/apps/frontend/src/App.tsx
@@ -1,9 +1,23 @@
-
+import phaser from 'phaser'
+import { BrowserRouter, createBrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 300,
+    backgroundColor: "#e2e"
+  };
+  let game = new phaser.Game(config)
+ 
 
+  console.log(game)
   return (
     <>
-      <h1 className="text-gray-600 text-5xl font-bold">hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
