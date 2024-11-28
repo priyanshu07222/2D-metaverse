a9dbb4b0d33f6db8c3bada4fb889a84f4223276f
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-11-28 14:01:56 +0530

    added camera in phaser

--- metaverse/apps/frontend/src/scenes/MainScene.ts
+++ metaverse/apps/frontend/src/scenes/MainScene.ts
@@ -1,18 +1,19 @@
 import Phaser from "phaser";
 
 const mainScene = (): Phaser.Types.Scenes.CreateSceneFromObjectConfig => {
+    let avatar: Phaser.Physics.Arcade.Sprite;
+    // let controls;
 
     function preload(this: Phaser.Scene) {
-        // Load assets
 
         this.load.image('floor', '/assets/Little_Bits_Office_Floors.png')
         this.load.image('objects', '/assets/Little_Bits_office_objects.png')
         this.load.image('walls', '/assets/Little_Bits_office_walls.png')
         this.load.tilemapTiledJSON('office-map', '/assets/officetiled')
 
-        this.load.spritesheet('character', '/assets/character.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('character', '/assets/character.png', { frameWidth: 17, frameHeight: 32 });
     }
-    let avatar: Phaser.Physics.Arcade.Sprite;
+
 
     function create(this: Phaser.Scene) {
         const map = this.make.tilemap({ key: 'office-map' });
@@ -22,27 +23,28 @@ const mainScene = (): Phaser.Types.Scenes.CreateSceneFromObjectConfig => {
         const objectsTileset = map.addTilesetImage('Little_Bits_office_objects', 'objects')
         const wallsTileset = map.addTilesetImage('Little_Bits_office_walls', 'walls')
 
-        const floorlayer = map.createLayer('floor', floorTileset!, 0, 0)
+        map.createLayer('floor', floorTileset!, 0, 0)
         const wallslayer = map.createLayer('walls', wallsTileset!, 0, 0)
         const objectslayer = map.createLayer('object', objectsTileset!, 0, 0)
         map.createLayer('things', objectsTileset!, 0, 0)
-        // avatar.setCollidesWith(0)
 
         wallslayer?.setCollisionByProperty({ collides: true }, true, true)
         objectslayer?.setCollisionByProperty({ collides: true })
 
-        // Add player/avatar
-        avatar = this.physics.add.sprite(100, 100, 'character', 0)
-        avatar.setCollideWorldBounds()
-        this.physics.add.collider(avatar, wallslayer!); // Collide with walls
-        this.physics.add.collider(avatar, objectslayer!); 
-        // avatar.setCollidesWith(0)
 
-        // Add collider for player and map
+        avatar = this.physics.add.sprite(100, 100, 'character')
+        avatar.setCollideWorldBounds()
+        this.physics.add.collider(avatar, wallslayer!);
+        this.physics.add.collider(avatar, objectslayer!);
+
+
+        const camera = this.cameras.main;
+        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        camera.startFollow(avatar)
+        camera.zoomTo(2, 1000)
     }
 
     function update(this: Phaser.Scene) {
-        // Handle player movement
         const cursors = this.input.keyboard.createCursorKeys();
 
 
@@ -64,7 +66,6 @@ const mainScene = (): Phaser.Types.Scenes.CreateSceneFromObjectConfig => {
             avatar.setVelocityX(0)
         } else {
             avatar.setVelocityY(0);
-            // avatar.setVelocity
         }
     }
 
--- metaverse/apps/frontend/src/utils/phaser.ts
+++ metaverse/apps/frontend/src/utils/phaser.ts
@@ -6,6 +6,12 @@ export const phaserGameConfig: Phaser.Types.Core.GameConfig = {
   width: 800,
   height: 600,
   backgroundColor: '#4rf',
+  scale: {
+    mode: Phaser.Scale.FIT, // Ensures the game fits within the container
+    autoCenter: Phaser.Scale.CENTER_BOTH, // Centers the canvas
+    width: window.innerWidth * 0.7, // 70% of the screen width
+    height: window.innerHeight * 0.7, // 70% of the screen height
+  },
   physics: {
     default: 'arcade',
     arcade: {
